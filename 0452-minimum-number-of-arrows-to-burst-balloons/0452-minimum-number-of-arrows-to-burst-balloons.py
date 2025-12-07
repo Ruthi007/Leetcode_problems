2f28052027dd65e25b31f267b8f3fa1274c8c3ba
SVN Ruthik <159603077+Ruthi007@users.noreply.github.com>
2025-12-08 00:07:38 +0530

    Time: 159 ms (25.52%), Space: 51.5 MB (8.46%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort()
+        arrows=len(points)
+        prev=points[0]
+        for i in range(1,len(points)):
+            curr=points[i]
+            if curr[0]<=prev[1]:    #Merge the overlapping ones
+                arrows-=1
+                prev=(curr[0],min(curr[1],prev[1])) 
+            else:
+                prev=curr
+        return arrows
